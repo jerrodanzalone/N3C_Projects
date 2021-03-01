53ff1acc1b3ab6589ebf768dfbbd5b71b6750d14
vector-client-token <>
2021-03-01 23:00:04 +0000

    Update workbook

--- pipeline.R
+++ pipeline.R
@@ -1,6 +1,14 @@
 
 
 @transform_pandas(
+    Output(rid="ri.vector.main.execute.1616db84-523f-444d-89f9-5596524357b0"),
+    hosp_covid_pts_comorb_smoke_vent=Input(rid="ri.foundry.main.dataset.a53be7a8-8182-4a7d-81e2-7008cdeb712e")
+)
+dead <- function(hosp_covid_pts_comorb_smoke_vent) {
+  d <-  count(hosp_covid_pts_comorb_smoke_vent[which date_of_death!=NULL],discharge_to_concept_name)
+}
+
+@transform_pandas(
     Output(rid="ri.foundry.main.dataset.fde7591f-1063-4f3f-a3f4-1a1a32cbdda2"),
     hosp_covid_pts_comorb_smoke_vent=Input(rid="ri.foundry.main.dataset.a53be7a8-8182-4a7d-81e2-7008cdeb712e")
 )
@@ -14,13 +22,5 @@ hrs_func <- function(hosp_covid_pts_comorb_smoke_vent) {
    i <- nrow(distinct(hosp_covid_pts_comorb_smoke_vent, person_id, macrovisit_start_date,macrovisit_end_date,discharge_to_concept_name,discharge_to_source_value))
    out <- data.frame(d,e,e1,f,g,h,i)
    return(out)
-}
-
-@transform_pandas(
-    Output(rid="ri.vector.main.execute.1616db84-523f-444d-89f9-5596524357b0"),
-    hosp_covid_pts_comorb_smoke_vent=Input(rid="ri.foundry.main.dataset.a53be7a8-8182-4a7d-81e2-7008cdeb712e")
-)
-unnamed <- function(hosp_covid_pts_comorb_smoke_vent) {
-    
 }
 
