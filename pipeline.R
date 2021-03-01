60081c0e57727fe0a2e4b8716eb628f0172795ea
vector-client-token <>
2021-03-01 23:03:08 +0000

    Update workbook

--- pipeline.R
+++ pipeline.R
@@ -5,7 +5,7 @@
     hosp_covid_pts_comorb_smoke_vent=Input(rid="ri.foundry.main.dataset.a53be7a8-8182-4a7d-81e2-7008cdeb712e")
 )
 dead <- function(hosp_covid_pts_comorb_smoke_vent) {
-  d <-  count(hosp_covid_pts_comorb_smoke_vent[which date_of_death!=NULL],discharge_to_concept_name)
+  d <-  count(hosp_covid_pts_comorb_smoke_vent[which(date_of_death!=NULL),],discharge_to_concept_name)
   out <- data.frame(d)
   return(out)
 }
