ac112bc031d44d738b82e5f60304953044cfddab
vector-client-token <>
2021-03-01 19:22:05 +0000

    Update workbook

--- pipeline.R
+++ pipeline.R
@@ -13,11 +13,3 @@ hrs_func <- function(hosp_covid_pts_comorb_smoke_vent) {
    return(out)
 }
 
-@transform_pandas(
-    Output(rid="ri.vector.main.execute.0e68970f-704e-4882-8419-74095b8cebe3"),
-    hosp_covid_pts_comorb_smoke_vent=Input(rid="ri.foundry.main.dataset.a53be7a8-8182-4a7d-81e2-7008cdeb712e")
-)
-unnamed <- function(hosp_covid_pts_comorb_smoke_vent) {
-    
-}
-
