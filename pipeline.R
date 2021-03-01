d25c5122b6151cd2261a1894adb39c831e8459bf
vector-client-token <>
2021-03-01 19:40:32 +0000

    Update workbook

--- pipeline.R
+++ pipeline.R
@@ -9,9 +9,10 @@ hrs_func <- function(hosp_covid_pts_comorb_smoke_vent) {
    e <- nrow(distinct(hosp_covid_pts_comorb_smoke_vent, person_id))
    e1 <- dim(distinct(hosp_covid_pts_comorb_smoke_vent, person_id, .keep_all=TRUE))
    f <- nrow(distinct(hosp_covid_pts_comorb_smoke_vent, person_id, macrovisit_start_date))
-   g <- nrow(distinct(hosp_covid_pts_comorb_smoke_vent, person_id, macrovisit_start_date,discharge_to_concept_name))
-   h <- nrow(distinct(hosp_covid_pts_comorb_smoke_vent, person_id, macrovisit_start_date,discharge_to_concept_name,discharge_to_source_value))
-   out <- data.frame(d,e,e1,f,g,h)
+   g <- nrow(distinct(hosp_covid_pts_comorb_smoke_vent, person_id, macrovisit_start_date,macrovisit_end_date))
+   h <- nrow(distinct(hosp_covid_pts_comorb_smoke_vent, person_id, macrovisit_start_date,macrovisit_end_date,discharge_to_concept_name))
+   i <- nrow(distinct(hosp_covid_pts_comorb_smoke_vent, person_id, macrovisit_start_date,macrovisit_end_date,discharge_to_concept_name,discharge_to_source_value))
+   out <- data.frame(d,e,e1,f,g,h,i)
    return(out)
 }
 
