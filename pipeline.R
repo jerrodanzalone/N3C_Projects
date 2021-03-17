66cf8fcb8529a947f5729337a8ea5a38cea2bbe0
vector-client-token <>
2021-03-17 01:03:58 +0000

    Update workbook

--- pipeline.R
+++ pipeline.R
@@ -31,8 +31,6 @@ hrs_func <- function(hosp_covid_pts_comorb_smoke_vent) {
     hosp_covid_pts_comorb_smoke_vent=Input(rid="ri.foundry.main.dataset.a53be7a8-8182-4a7d-81e2-7008cdeb712e")
 )
 unnamed <- function(hosp_covid_pts_comorb_smoke_vent) {
-    
-  out = hosp_covid_pts_comorb_smoke_vent
-  return(out)
+
 }
 
